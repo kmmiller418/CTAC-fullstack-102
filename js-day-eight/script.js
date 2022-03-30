1ffa47d73eb4ca22ce2b22b103c64b47b92d463e
kmiller <kmmiller418@gmail.com>
2022-03-30 11:28:22 -0400

    Complete traversal assignment

---
+++ js-day-eight/script.js
@@ -0,0 +1,20 @@
+const header = document.querySelector('header');
+console.log(header);
+
+const sections = document.getElementsByTagName('section');
+console.log(sections);
+
+const currentSection = document.querySelector('section.current');
+console.log(currentSection);
+
+const sectionAfterCurrent = currentSection.nextElementSibling;
+console.log(sectionAfterCurrent);
+
+const h2BeforeCurrent = currentSection.previousElementSibling.childNodes[1];
+console.log(h2BeforeCurrent);
+
+const divOfParentOfHighlight = document.querySelector('h2.highlight').parentElement.parentElement;
+console.log(divOfParentOfHighlight);
+
+const sectionsWithH2s = document.querySelectorAll('section h2')
+console.log(sectionsWithH2s);
