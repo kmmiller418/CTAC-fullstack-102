7cc223322c9deb61a571d7f1489807e1b355e4e2
kmiller <kmmiller418@gmail.com>
2022-03-31 14:13:52 -0400

    Add GET post 10 button event listener function, create post button on html

--- ajax-rest-promises/script.js
+++ ajax-rest-promises/script.js
@@ -1,25 +1,51 @@
 // Button selectors:
 let postButton = document.getElementById("get-posts");
 let post10Button = document.getElementById("get-post-10");
+let createButton = document.getElementById("create-post");
 
+//post container
 let posts = document.getElementById("post-container");
-let post = document.getElementById("post");
-let postID = document.getElementById("post-id");
-let postTitle = document.getElementById("post-title");
-let userId = document.getElementById("user-id");
 
 //Event listeners:
-postButton.addEventListener("click", (e) => {
+postButton.addEventListener("click", () => {
+  posts.innerHTML = "";
+
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
     .then((json) => {
-        
-        for (i = 0; i < json.length-1; i++){
-            post.innerHTML = json[i].body;
-            postID.innerHTML = json[i].id;
-            postTitle.innerHTML = json[i].title;
-            userId.innerHTML = json[i].userId;
-        }
+      for (i = 0; i < json.length - 1; i++) {
+        let post = document.createElement("p");
+        let postID = document.createElement("p");
+        let postTitle = document.createElement("p");
+        let userId = document.createElement("p");
+
+        post.innerHTML = json[i].body;
+        postID.innerHTML = json[i].id;
+        postTitle.innerHTML = json[i].title;
+        userId.innerHTML = json[i].userId;
+
+        posts.append(post, postID, postTitle, userId);
+      }
+    });
+});
+
+post10Button.addEventListener("click", () => {
+  posts.innerHTML = "";
+
+  fetch("https://jsonplaceholder.typicode.com/posts/10")
+    .then((response) => response.json())
+    .then((json) => {
+      let post = document.createElement("p");
+      let postID = document.createElement("p");
+      let postTitle = document.createElement("p");
+      let userId = document.createElement("p");
+
+      post.innerHTML = json.body;
+      postID.innerHTML = json.id;
+      postTitle.innerHTML = json.title;
+      userId.innerHTML = json.userId;
+
+      posts.append(post, postID, postTitle, userId);
     });
 });
 
