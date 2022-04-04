e8e89baabd65a57a96522bc30093854b84c24770
kmiller <kmmiller418@gmail.com>
2022-04-04 15:55:38 -0400

    Add google cdn to html file, add form manipulation to script

---
+++ jquery-form-validation/script.js
@@ -0,0 +1,28 @@
+//$(document).ready deprecated, documentation suggested using jQuery(function) instead
+
+jQuery(function () {
+  $("#submit").on("click", function () {
+    let name = $("#name");
+    let email = $("#email");
+    let phoneNum = $("#phone");
+    let required = new Array(name, email, phoneNum);
+    let message = $("#message").html("Please Fill Out Required Fields");
+
+    for (i = 0; i < required.length; i++) {
+      if (required[i].val() == "") {
+        message.addClass("warning");
+        required[i].prev().addClass("warning");
+      }
+
+      if (required[i].val() != "") {
+        message.removeClass("warning");
+        required[i].prev().removeClass("warning");
+      }
+    }
+
+    if ($(".warning").length == 0) {
+      $("#form").remove();
+      $("h2").html("Thanks for your feedback!");
+    }
+  });
+});
