951d0fc1e652a82e30ff5fc966e09babab6d243c
kmiller <kmmiller418@gmail.com>
2022-04-06 11:13:28 -0400

    Fix get single resource route

--- npm-phonebook/index.js
+++ npm-phonebook/index.js
@@ -38,7 +38,8 @@ app.get("/api/persons", (request, response) => {
 
 app.get("/api/persons/:id", (request, response) => {
     const id = Number(request.params.id);
-    persons.filter((person) => person.id === id)
+    const person = persons.find((person) => person.id === id)
+    console.log(person);
 
     if (person){
         response.json(person);
@@ -46,6 +47,8 @@ app.get("/api/persons/:id", (request, response) => {
         response.status(404).end();
     }
 });
+
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
